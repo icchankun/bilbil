f79d9c5c95d2cbf78cdfe50c07026e2fe1e8c2f3
icchan-webapp <ikkun.official@gmail.com>
2022-09-27 21:19:10 +0900

    refs #1 feat: 管理者がログイン・ログアウトをした際に遷移するページの設定 ログイン→トークテーマ一覧ページ ログアウト→管理者ログインページ

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -24,4 +24,12 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource)
+    admin_talk_themes_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 end
