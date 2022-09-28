a9b0f59c166fc12ee9c810ce3df505f6be5a6599
icchan-webapp <ikkun.official@gmail.com>
2022-09-28 14:28:54 +0900

    refs #3 feat: categoriesに関するルーティングを追加 formatはjsonに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    namespace :api do
-      namespace :v1 do
-        get 'categories/index'
-      end
-    end
-  end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
   namespace :admin do
     resources :talk_themes, only: [:index, :create, :edit, :update, :destroy]
+    namespace :api, {format: 'json'} do
+      namespace :v1 do
+        resources :categories, only: [:index, :create, :edit, :update, :destroy]
+      end
+    end
   end
 end
