6923a0ace3f32937b5dcd8d02d05415de4736cfe
icchan-webapp <ikkun.official@gmail.com>
2022-09-28 15:53:53 +0900

    refs #3 feat: categoriesコントローラのindexアクションに記述を追加 ログインをしていないと、アクションが動かないようにし、 ActiveRecordのレコードが見つからなければ404 not foundを応答するようにした

--- app/controllers/admin/api/v1/categories_controller.rb
+++ app/controllers/admin/api/v1/categories_controller.rb
@@ -1,4 +1,13 @@
 class Admin::Api::V1::CategoriesController < ApiController
+  before_action :authenticate_admin!
+
+  # ActiveRecordのレコードが見つからなければ404 not foundを応答する
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { error: '404 not found' }, status: 404
+  end
+
   def index
+    categories = Category.all
+    render json: categories
   end
 end
