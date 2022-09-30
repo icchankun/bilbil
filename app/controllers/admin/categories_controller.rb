ff5532a57d79cdea4a317b36c83f8413a8974ac8
icchan-webapp <ikkun.official@gmail.com>
2022-09-30 22:59:43 +0900

    refs #5 feat: トークテーマカテゴリーの一覧ページを作成 その際に、カテゴリー一覧表示とカテゴリー新規作成フォームを追加

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,19 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to admin_categories_path
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
