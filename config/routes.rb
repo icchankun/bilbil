1852b58e505135719b35bc900f55de951325d18a
icchan-webapp <ikkun.official@gmail.com>
2022-09-30 21:52:53 +0900

    refs #5 feat: admin/categoriesに関するルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'categories/index'
-  end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
   namespace :admin do
+    resources :categories, only: [:index, :create, :edit, :update, :destroy]
     resources :talk_themes, only: [:index, :create, :edit, :update, :destroy]
   end
   namespace :api, {format: 'json'} do
