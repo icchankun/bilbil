3d8c58486c5f8917fc2b31f5c5a7bcef7739e135
icchan-webapp <ikkun.official@gmail.com>
2022-10-05 11:02:52 +0900

    refs #8 feat: 管理者トップページに関するコントローラ & アクションを作成 コントローラ→admin/homeコントローラ アクション→topアクション

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
