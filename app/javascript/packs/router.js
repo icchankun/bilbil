79a8bee376e6206f9212bb624e8514a127a7da08
icchan-webapp <ikkun.official@gmail.com>
2022-10-09 00:58:40 +0900

    refs #14 feat: router.jsにContentNewPage.vueを追加

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import ContentIndexPage from "../admin/ContentIndexPage.vue";
+import ContentNewPage from "../admin/ContentNewPage.vue";
 
 const routes = [
-  { path: "/", component: ContentIndexPage },
+  { path: "/", 
+    component: ContentIndexPage },
+  { path: "/homes/new", 
+    component: ContentNewPage },
 ]
 
 const router = createRouter({
