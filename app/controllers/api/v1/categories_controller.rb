1ed7e111a92d81a0e8ceb272900ee2690c80f20f
icchan-webapp <ikkun.official@gmail.com>
2022-10-12 11:35:57 +0900

    refs #14 feat: categoriesコントローラにcreateアクションの記述を追加

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -10,4 +10,18 @@ class Api::V1::CategoriesController < ApiController
     categories = Category.all
     render json: categories, each_serializer: CategorySerializer
   end
+
+  def create
+    category =  Category.new(category_params)
+    if category.save
+      render json:  category, status: :created
+    else
+      render json: { errors:  category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
