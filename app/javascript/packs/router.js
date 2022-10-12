8953904c89e66d113465d452f6997a035d0ea319
icchan-webapp <ikkun.official@gmail.com>
2022-10-13 01:05:25 +0900

    refs #17 feat: CategoryEditPage.vueを追加し、ルーティングを設定

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import ContentIndexPage from "../admin/ContentIndexPage.vue";
 import ContentNewPage from "../admin/ContentNewPage.vue";
+import CategoryEditPage from "../admin/CategoryEditPage.vue";
 
 const routes = [
   { path: "/", 
     component: ContentIndexPage },
   { path: "/homes/new", 
     component: ContentNewPage },
+  { path: '/categories/:id(\\d+)/edit',
+    name: 'CategoryEditPage',
+    component: CategoryEditPage  }
 ]
 
 const router = createRouter({
