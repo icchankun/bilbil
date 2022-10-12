f0bd558e340907e99c48cb3a499a8a58f7cc2c8c
icchan-webapp <ikkun.official@gmail.com>
2022-10-12 11:44:39 +0900

    refs #14 feat: talk_themesコントローラにcreateアクションの記述を追加 その際に、talk_themesのルーティングにcreateを追加

--- app/controllers/api/v1/talk_themes_controller.rb
+++ app/controllers/api/v1/talk_themes_controller.rb
@@ -10,4 +10,18 @@ class Api::V1::TalkThemesController < ApiController
     talk_themes = TalkTheme.all
     render json: talk_themes, each_serializer: TalkThemeSerializer
   end
+
+  def create
+    talk_theme =  TalkTheme.new(talk_theme_params)
+    if talk_theme.save
+      render json:  talk_theme, status: :created
+    else
+      render json: { errors:  talk_theme.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def talk_theme_params
+      params.require(:talk_theme).permit(:content, :category_id)
+    end
 end
