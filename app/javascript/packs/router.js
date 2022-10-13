4c9cb1a9f2ee90d0a7c8d326b7f01a31f1e1a9a3
icchan-webapp <ikkun.official@gmail.com>
2022-10-13 23:12:01 +0900

    refs #17 feat: TalkThemeEditPage.vueを追加し、ルーティングを設定

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import ContentIndexPage from "../admin/ContentIndexPage.vue";
 import ContentNewPage from "../admin/ContentNewPage.vue";
 import CategoryEditPage from "../admin/CategoryEditPage.vue";
+import TalkThemeEditPage from "../admin/TalkThemeEditPage.vue";
 
 const routes = [
   { path: "/", 
@@ -10,7 +11,10 @@ const routes = [
     component: ContentNewPage },
   { path: '/categories/:id(\\d+)/edit',
     name: 'CategoryEditPage',
-    component: CategoryEditPage  }
+    component: CategoryEditPage  },
+  { path: '/talk_themes/:id(\\d+)/edit',
+    name: 'TalkThemeEditPage',
+    component: TalkThemeEditPage  },
 ]
 
 const router = createRouter({
