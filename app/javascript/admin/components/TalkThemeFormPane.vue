9dfdb76c53acb099ef21c9863432b3d936f9e9b4
icchan-webapp <ikkun.official@gmail.com>
2022-10-16 11:23:25 +0900

    refs #20 feat: トークテーマ・カテゴリーの新規登録及び編集フォームをコンポーネント化

---
+++ app/javascript/admin/components/TalkThemeFormPane.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="onSubmit()">
+    <error-message-display :errors="errors"></error-message-display>
+    <div><input v-model="talk_theme.content" type="text" /> ?</div>
+    <div>
+      <select v-model="talk_theme.category_id">
+        <option disabled value="">選択してください</option>
+        <option
+          v-for="category in categories"
+          :value="category.id"
+          :key="category.id"
+        >
+          {{ category.name }}
+        </option>
+      </select>
+    </div>
+    <button type="submit">トークテーマを作成</button>
+  </form>
+</template>
+
+<script>
+import ErrorMessageDisplay from "./ErrorMessageDisplay.vue";
+
+export default {
+  components: {
+    ErrorMessageDisplay,
+  },
+  props: {
+    talk_theme: {},
+    categories: {},
+    errors: "",
+  },
+  emits: ["submit"],
+  methods: {
+    onSubmit() {
+      this.$emit("submit");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
