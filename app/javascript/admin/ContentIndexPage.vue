4bbdb7f374a66e90f69279539c13b98db832ee4b
icchan-webapp <ikkun.official@gmail.com>
2022-10-19 20:57:45 +0900

    refs #24 style:  管理者側のトークテーマ・カテゴリー一覧画面のデザインを変更

--- app/javascript/admin/ContentIndexPage.vue
+++ app/javascript/admin/ContentIndexPage.vue
@@ -1,29 +1,83 @@
 <template>
   <Header>C O N T E N T</Header>
-  <div class="container">
-    <a class="d-block py-1" data-method="delete" href="/admin/sign_out">ログアウトする。</a>
-    <router-link class="d-block py-1" to="/content/new"
-      >トークテーマ・カテゴリーの作成へ。</router-link
-    >
-    <div v-for="category in categories" :key="category.id">
-      <h3>
-        {{ category.name }}
-        <router-link
-          :to="{ name: 'CategoryEditPage', params: { id: category.id } }"
-          >編集</router-link
-        >
-        <button @click="deleteCategory(category.id)">削除</button>
-      </h3>
-      <div v-for="talk_theme in category.talk_themes" :key="talk_theme.id">
-        {{ talk_theme.content }}
-        <router-link
-          :to="{ name: 'TalkThemeEditPage', params: { id: talk_theme.id } }"
-          >編集</router-link
-        >
-        <button @click="deleteTalkTheme(talk_theme.id)">削除</button>
+  <main class="container">
+    <div class="row my-4">
+      <div class="col-lg-6 col-sm-12 mx-auto">
+        <div class="row my-2">
+          <a class="d-block py-1" data-method="delete" href="/admin/sign_out"
+            >ログアウトする。</a
+          >
+          <router-link class="d-block py-1" to="/content/new"
+            >トークテーマ・カテゴリーの作成へ。</router-link
+          >
+        </div>
+        <div class="row my-2">
+          <div
+            class="dropdown"
+            v-for="category in categories"
+            :key="category.id"
+          >
+            <button
+              type="button"
+              class="btn btn-dark text-start w-100 dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <dl class="d-inline-flex flex-wrap m-0">
+                <dt class="w-25 me-3 text-white">{{ category.name }}</dt>
+                <dd class="me-auto">
+                  <router-link
+                    class="btn btn-success me-2"
+                    :to="{
+                      name: 'CategoryEditPage',
+                      params: { id: category.id },
+                    }"
+                    >編集</router-link
+                  >
+                  <a class="btn btn-danger" @click="deleteCategory(category.id)"
+                    >削除</a
+                  >
+                </dd>
+                <dd class="me-1">
+                  トークテーマ数: {{ category.talk_themes.length }}
+                </dd>
+              </dl>
+            </button>
+            <ol
+              class="dropdown-menu"
+            >
+              <li class="dropdown-item" v-for="talk_theme in category.talk_themes"
+              :key="talk_theme.id" v-if="category.talk_themes.length != 0">
+                <dl class="d-inline-flex flex-wrap m-0">
+                  <dt class="w-75 me-auto mb-2 text-wrap">{{ talk_theme.content }} ?</dt>
+                  <dd>
+                    <router-link
+                      class="btn btn-success me-2"
+                      :to="{
+                        name: 'TalkThemeEditPage',
+                        params: { id: talk_theme.id },
+                      }"
+                      >編集</router-link
+                    >
+                    <a
+                      class="btn btn-danger"
+                      @click="deleteTalkTheme(talk_theme.id)"
+                      >削除</a
+                    >
+                  </dd>
+                </dl>
+              </li>
+              <li class="d-block px-1 py-2" v-else>
+                <dl class="m-0">
+                  <dt>トークテーマはありません。</dt>
+                </dl>
+              </li>
+            </ol>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </main>
   <Footer></Footer>
 </template>
 
@@ -93,8 +147,21 @@ export default {
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: left;
+.dropdown-toggle {
+  border-radius: 0;
+  border-bottom: 1px solid #fff;
+}
+.dropdown-menu {
+  position: static !important;
+  transform: translate(0, 0) !important;
+  border-radius: 0; /* border-radius: 0; */
+}
+
+.dropdown-item {
+  display: list-item;
+  list-style: decimal inside;
+}
+.d-inline-flex {
+  width: 95%;
 }
 </style>
