687ffca22415052409732347bf48f6c5403ec0f2
icchan-webapp <ikkun.official@gmail.com>
2022-10-20 22:25:52 +0900

    refs #24 feat: ヘッドラインコンポーネントを追加

---
+++ app/javascript/components/Headline.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mb-3 text-center">
+    <h3 class="d-inline-block bg-dark p-1 text-white">
+      <slot></slot>
+    </h3>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+  h3 {
+    width: 200px;
+  }
+</style>
