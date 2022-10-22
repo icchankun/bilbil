c6f2444cd9088a7f5013f215c9742214b4f5caaf
icchan-webapp <ikkun.official@gmail.com>
2022-10-22 11:23:39 +0900

    refs #24 feat: 管理者側のトークテーマ・カテゴリー一覧画面に遷移するためのボタンを作成 また、そのボタンはコンポーネントにしている。

---
+++ app/javascript/components/ContentIndexButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center">
+    <router-link class="btn btn-dark" to="/"
+      >コンテンツ一覧に戻る</router-link
+    >
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
