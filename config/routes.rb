42d065c494565a5af8053235eb7eb30d3ec01a79
icchan-webapp <ikkun.official@gmail.com>
2022-10-22 21:38:53 +0900

    refs #27 feat: エンドユーザー側のルートパスを設定。

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-
+  
   namespace :admin do
     root 'homes#top'
   end
   
+  scope module: :end_user do
+    root 'homes#top'
+  end
+
   namespace :api, {format: 'json'} do
     namespace :v1 do
       resources :categories, only: [:index, :create, :show, :update, :destroy]
