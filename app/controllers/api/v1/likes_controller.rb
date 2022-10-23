de88f60acfd749fef09aa8b46a389f0b981de8fb
icchan-webapp <ikkun.official@gmail.com>
2022-10-23 21:03:18 +0900

    refs #29 feat: likesのapiコントローラを作成。

---
+++ app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::LikesController < ApiController
+
+  # ActiveRecordのレコードが見つからなければ404 not foundを応答する
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { error: '404 not found' }, status: 404
+  end
+
+  def create
+  end
+
+  def destory
+  end
+end
