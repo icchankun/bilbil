8da5d9781eede21547236107d9ce36969326e51f
icchan-webapp <ikkun.official@gmail.com>
2022-11-03 18:38:39 +0900

    refs #36 feat: urlに#がつかないよう変更。 createWebHushHistoryをcreateWebHistoryに変更。 その際に、存在しないurlが入力された場合、ルートパスに遷移するよう変更（本番環境のみ）。

--- app/javascript/packs/router/end-user-router.js
+++ app/javascript/packs/router/end-user-router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import TalkThemeRoulettePage from "../../end_user/TalkThemeRoulettePage.vue";
 import ContentIndexPage from "../../end_user/ContentIndexPage.vue";
 
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
