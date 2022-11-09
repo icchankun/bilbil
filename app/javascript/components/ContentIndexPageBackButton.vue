7d974cf2d6f8caa9c2f5a20b75504e211b30e2ff
icchan-webapp <ikkun.official@gmail.com>
2022-11-09 15:40:31 +0900

    feat: RootPathButtonを変更。 管理者側のコンテンツ一覧画面に戻るボタンとルーレット画面に戻るボタンに分けた。

--- app/javascript/components/ContentIndexPageBackButton.vue
+++ app/javascript/components/ContentIndexPageBackButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center">
-    <router-link class="btn btn-dark" to="/"
-      ><slot></slot></router-link
+    <router-link class="btn btn-dark" to="/admin"
+      >コンテンツ一覧に戻る</router-link
     >
   </div>
 </template>
