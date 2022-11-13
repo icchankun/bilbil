c8c2e8af2be08f4f302ee1ba8cd041b61e1585f7
icchan-webapp <ikkun.official@gmail.com>
2022-11-13 12:39:18 +0900

    refs #48 refactor: app.vueをマウントするための記述を一つにまとめる。 今まで、今までは管理者とエンドユーザーでapp.vueをマウントするための記述を分けていたが、その必要がなくなったため、統合した。

--- app/javascript/packs/admin.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "../app.vue";
-import Router from "./router/admin-router.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const app = createApp(App);
-  app.use(Router);
-  app.mount("#vue-app");
-});
