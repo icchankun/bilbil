57fc49c87293a3d1bb89ace0c468662372483d55
icchan-webapp <ikkun.official@gmail.com>
2022-11-22 22:58:15 +0900

    refs #50 feat: 各モデルファイルにバリデーションを追加。

--- app/models/talk_theme.rb
+++ app/models/talk_theme.rb
@@ -1,5 +1,5 @@
 class TalkTheme < ApplicationRecord
   belongs_to :category
   has_many :likes, dependent: :destroy
-  validates :content, presence: true
+  validates :content, presence: true, length: { in: 4..30 }, uniqueness: true
 end
