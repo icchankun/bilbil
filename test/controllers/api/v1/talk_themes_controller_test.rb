2adcf7364b8497ea4d5b4dbdb7c4113e8aa0d624
icchan-webapp <ikkun.official@gmail.com>
2022-11-23 16:39:03 +0900

    refs #51 refactor: RSpecを用いるため、不要となったtestフォルダを削除。

--- test/controllers/api/v1/talk_themes_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Api::V1::TalkThemesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_v1_talk_themes_index_url
-    assert_response :success
-  end
-end
