91a2477a1788b31dec5d9bd6d090222e44efe5a0
icchan-webapp <ikkun.official@gmail.com>
2022-11-25 22:52:05 +0900

    refs #51 feat: トークテーマ・カテゴリーのCRUD機能をテストするためのファイルを作成

---
+++ spec/requests/crud_of_categories_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe "CrudOfCategories", type: :request do
+end
---
+++ spec/requests/crud_of_talk_themes_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe "CrudOfTalkThemes", type: :request do
+end
