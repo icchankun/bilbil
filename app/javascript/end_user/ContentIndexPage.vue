0d6e63a44e46eef840c0e55de6c01fae1a30e6e4
icchan-webapp <ikkun.official@gmail.com>
2022-12-09 19:32:42 +0900

    refs #56 refactor: いいねをしたトークテーマのidの配列を操作する関数を定義。 いいねをしたトークテーマのidの配列に新たなトークテーマのidの追加といいねを解除したトークテーマのidの削除を行うための関数を定義。 その際にソースコードの説明をコメントアウトで記入。

--- app/javascript/end_user/ContentIndexPage.vue
+++ app/javascript/end_user/ContentIndexPage.vue
@@ -1,8 +1,9 @@
 <template>
   <Header>C O N T E N T</Header>
   <main class="container">
-    <div class="row my-5">
+    <div class="row mx-2 my-5">
       <div class="col-lg-7 mx-auto">
+        <!-- ページ説明文 -->
         <div class="mb-3">
           <div class="mb-2">
             このアプリで出題されるトークテーマを各カテゴリーごと
@@ -11,17 +12,23 @@
           <div class="mb-2">
             また、トークテーマにいいねをすることができます。
           </div>
-          <div class="mb-2">
+          <div>
             いいねをしたトークテーマは各カテゴリーのトークテーマ一覧の上部に表示されます。
           </div>
         </div>
-        <div class="mb-3 text-center">
-          <router-link class="py-1" to="/guide"
-            >ご利用ガイドはこちらから。</router-link
-          >
+        <!-- /ページ説明文 -->
+        <!-- ページ遷移リンク -->
+        <div class="mb-3">
+          <div class="mb-2 text-center">
+            <router-link class="py-1" to="/guide"
+              >ご利用ガイドはこちらから。</router-link
+            >
+          </div>
+          <roulette-page-back-button></roulette-page-back-button>
         </div>
-        <roulette-page-back-button></roulette-page-back-button>
-        <div class="mt-5">
+        <!-- /ページ遷移リンク -->
+        <!-- 各カテゴリーのトークテーマ一覧 -->
+        <div class="mb-3">
           <div
             class="row dropdown"
             v-for="category in categories"
@@ -29,7 +36,7 @@
           >
             <button
               type="button"
-              class="btn btn-dark dropdown-toggle py-2"
+              class="btn btn-dark dropdown-toggle"
               data-bs-toggle="dropdown"
               data-bs-auto-close="false"
             >
@@ -39,33 +46,35 @@
             </button>
             <ol class="dropdown-menu p-0">
               <li
-                v-for="(talk_theme, index) in category.talk_themes"
-                :key="talk_theme.id"
                 v-if="category.talk_themes.length != 0"
+                v-for="talk_theme in category.talk_themes"
+                :key="talk_theme.id"
                 class="dropdown-item"
               >
-                <dl class="row d-inline-flex flex-wrap m-0">
-                  <dt class="col-12 col-sm-11 text-wrap">
+                <div class="row d-inline-flex flex-wrap">
+                  <div class="col-9 fw-bold text-wrap">
                     {{ talk_theme.content }} ?
-                  </dt>
-                  <dd class="col-1">
+                  </div>
+                  <div class="col-3">
                     <talk-theme-like-button
                       :talk_theme_id="talk_theme.id"
                       :likes="talk_theme.likes"
-                      @fetchCategories="fetchCategories"
-                      @fetchLikesByIpAddress="fetchLikesByIpAddress"
+                      :liked_talk_theme_ids="this.liked_talk_theme_ids"
+                      @addLikedTalkTheme="addLikedTalkTheme"
+                      @removeLikedTalkTheme="removeLikedTalkTheme"
                     ></talk-theme-like-button>
-                  </dd>
-                </dl>
+                  </div>
+                </div>
               </li>
-              <li class="d-block px-1 py-2" v-else>
-                <dl class="m-0">
-                  <dt>トークテーマはありません。</dt>
-                </dl>
+              <li v-else>
+                <div class="fw-bold p-2">
+                  トークテーマはありません。
+                </div>
               </li>
             </ol>
           </div>
         </div>
+        <!-- /各カテゴリーのトークテーマ一覧 -->
       </div>
     </div>
   </main>
@@ -90,18 +99,27 @@ export default {
     RoulettePageBackButton,
     EndUserFooter,
   },
+  created() {
+    this.fetchCategories();
+    this.fetchLikedTalkThemeIds();
+  },
   data() {
     return {
-      categories: {},
-      liked_talk_theme_ids: [],
+      categories: [], // 全カテゴリーのデータの配列。
+      liked_talk_theme_ids: [], // いいねをしたトークテーマのidの配列。
     };
   },
-  created() {
-    this.fetchLikesByIpAddress();
-    this.fetchCategories();
+  watch: {
+    //いいねをしたトークテーマのidの配列に変化があった時、トークテーマの並び替えを行う。
+    liked_talk_theme_ids: {
+      handler() {
+        this.sortedTalkThemesByLikes;
+      },
+      deep: true,
+    },
   },
   computed: {
-    // カテゴリー一覧の順番を並び替える。
+    // いいねをしたトークテーマをトークテーマ一覧の上部に表示させる。
     sortedTalkThemesByLikes() {
       const ids = this.liked_talk_theme_ids;
 
@@ -119,19 +137,31 @@ export default {
     },
   },
   methods: {
-    // カテゴリー一覧を取得し、その順番を並び替える。
+    // 全カテゴリーのデータを取得し、トークテーマの並び替えを行う。
     fetchCategories() {
       axios.get("/api/v1/categories").then((response) => {
         this.categories = response.data;
-        this.categories = this.sortedTalkThemesByLikes();
+        this.sortedTalkThemesByLikes;
       });
     },
 
-    // 接続しているipアドレスがipカラムに保存されているいいねを取得する。
-    fetchLikesByIpAddress() {
+    // 接続しているipアドレスをipカラムに保存しているいいねレコードのトークテーマidカラムの値を配列にし、取得する。
+    
+    fetchLikedTalkThemeIds() {
       axios.get("/api/v1/like/ip").then((response) => {
         this.liked_talk_theme_ids = response.data;
       });
+    },
+
+    // いいねをしたトークテーマのidの配列に、新たにいいねをしたトークテーマのidを追加する。
+    addLikedTalkTheme(talk_theme_id) {
+      this.liked_talk_theme_ids.push(talk_theme_id);
+    },
+
+    // いいねをしたトークテーマのidの配列から、いいねを解除したトークテーマのidを削除する。
+    removeLikedTalkTheme(talk_theme_id) {
+      const ids = this.liked_talk_theme_ids.filter((id) => id != talk_theme_id);
+      this.liked_talk_theme_ids = ids;
     },
   },
 };
