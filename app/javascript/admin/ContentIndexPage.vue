db487714167597c45dc4421c54b336eb217cd989
icchan-webapp <ikkun.official@gmail.com>
2023-03-05 12:42:15 +0900

    refs #66 feat: 管理者側にトークテーマ検索フォームを設置。

--- app/javascript/admin/ContentIndexPage.vue
+++ app/javascript/admin/ContentIndexPage.vue
@@ -19,7 +19,7 @@
         <!-- /ページ遷移リンク -->
         <!-- カテゴリー選択ボタン -->
         <div class="mb-1">カテゴリーを選んでください。</div>
-        <div class="d-flex flex-wrap mb-4">
+        <div class="d-flex flex-wrap mb-3">
           <div
             class="me-2 mb-1"
             v-for="(category, index) in categories"
@@ -41,6 +41,13 @@
           </div>
         </div>
         <!-- /カテゴリー選択ボタン -->
+        <!-- トークテーマ検索フォーム-->
+        <talk-theme-search-form
+          :talk_themes="talk_themes"
+          @separateTalkThemesByCategory="separateTalkThemesByCategory"
+          @getFilteredTalkThemes="getFilteredTalkThemes"
+        ></talk-theme-search-form>
+        <!-- /トークテーマ検索フォーム-->
         <!-- 各カテゴリーのトークテーマ一覧 -->
         <div class="row bg-dark text-white fw-bold list_headline">
           <div class="col-12 col-sm-6">
@@ -55,19 +62,21 @@
           </div>
           <div class="col-12 col-sm-6 text-sm-end">
             <a
-              v-if="talk_themes.length == 0"
+              v-if="filtered_talk_themes.length == 0"
               class="btn btn-danger btn-sm"
-              @click="deleteCategory(category_id)"
+              @click="deleteCategory()"
               >カテゴリーを削除</a
             >
-            <span v-else>トークテーマ数: {{ talk_themes.length }}</span>
+            <span v-else
+              >トークテーマ数: {{ filtered_talk_themes.length }}</span
+            >
           </div>
         </div>
         <ol class="list">
           <!-- トークテーマが存在する場合 -->
           <div
-            v-if="talk_themes.length != 0"
-            v-for="(talk_theme, index) in talk_themes"
+            v-if="filtered_talk_themes.length != 0"
+            v-for="(talk_theme, index) in filtered_talk_themes"
             :key="talk_theme.id"
             class="row mb-2"
           >
@@ -109,58 +118,66 @@
 import axios from "axios";
 
 import Header from "../components/Header.vue";
+import TalkThemeSearchForm from "../components/TalkThemeSearchForm.vue";
 import AdminFooter from "../components/AdminFooter.vue";
 
 export default {
   components: {
     Header,
+    TalkThemeSearchForm,
     AdminFooter,
+  },
+  created() {
+    this.categoryDefault();
   },
   data() {
     return {
       categories: [], // 全カテゴリーのデータの配列。
-      talk_themes: [], // 選択したカテゴリーのトークテーマの配列。
       category_id: 0, // 選択されたカテゴリーのid。
       category_name: "", // 選択されたカテゴリーのカテゴリー名
+      talk_themes: [], // 選択したカテゴリーのトークテーマの配列。
+      filtered_talk_themes: [], // 選択したカテゴリーのトークテーマの配列。
     };
   },
   watch: {
     // 選択したカテゴリーが変わるごとに、ルーレットの内容と取得するカテゴリー名を変更する。
-    category_id: function () {
-      this.talkThemes();
+    category_id: async function () {
+      await this.separateTalkThemesByCategory();
+      this.sortTalkThemesByPopularity;
     },
-  },
-  created() {
-    // 全カテゴリーのデータを取得し、いいね数が多い順にトークテーマを並び替える。
-    axios.get("/api/v1/categories").then((response) => {
-      this.categories = response.data;
-      this.category_id = this.categories[0].id;
-      this.sortedTalkThemesByLikes;
-    });
   },
   computed: {
     // トークテーマをいいね数が多い順に並び替える。
-    sortedTalkThemesByLikes() {
-      this.categories.forEach((category) => {
-        category.talk_themes.sort((a, b) => {
-          return b.likes.length - a.likes.length;
-        });
+    sortTalkThemesByPopularity() {
+      this.filtered_talk_themes.sort((a, b) => {
+        return b.likes.length - a.likes.length;
       });
     },
   },
   methods: {
+    // 全カテゴリーのデータを取得する。
+    async fetchCategories() {
+      await axios.get("/api/v1/categories").then((response) => {
+        this.categories = response.data;
+      });
+    },
+
+    async categoryDefault() {
+      await this.fetchCategories();
+
+      // 選択されたカテゴリーの初期値をカテゴリーデータの先頭にする。
+      this.category_id = this.categories[0].id;
+    },
+
     // 特定のカテゴリーを削除する。
-    deleteCategory(delete_id) {
+    deleteCategory() {
       if (
         confirm(
-          `「${this.categoryName(
-            delete_id
-          )}」カテゴリーを削除してもよろしいですか?`
+          `「${this.category_name}」カテゴリーを削除してもよろしいですか?`
         )
       )
-        axios.delete(`/api/v1/categories/${delete_id}`).then(() => {
-          this.updateContents();
-          this.category_id = this.categories[0].id;
+        axios.delete(`/api/v1/categories/${this.category_id}`).then(() => {
+          this.categoryDefault();
         });
     },
 
@@ -172,41 +189,39 @@ export default {
         )
       )
         axios.delete(`/api/v1/talk_themes/${delete_id}`).then(() => {
-          this.updateContents();
+          this.filtered_talk_themes = this.filtered_talk_themes.filter(
+            (talk_theme) => talk_theme.id != delete_id
+          );
         });
-    },
-
-    // 削除したいカテゴリーのカテゴリー名を取得する。
-    categoryName(delete_id) {
-      const filterDate = this.categories.filter(
-        (category) => category.id === delete_id
-      );
-      return filterDate[0].name;
     },
 
     // 削除したいトークテーマの内容を取得する。
     talkThemeContent(delete_id) {
-      const filterDate = this.talk_themes.filter(
-        (talk_theme) => talk_theme.id === delete_id
+      const filtered_date = this.talk_themes.find(
+        (talk_theme) => talk_theme.id == delete_id
       );
-      return filterDate[0].content;
-    },
-
-    // 最新の全カテゴリーのデータを取得する。
-    updateContents() {
-      axios.get("/api/v1/categories").then((response) => {
-        this.categories = response.data;
-        this.talkThemes();
-      });
+      return filtered_date.content;
     },
 
     // 選択したカテゴリーのルーレット内容を配列にする。
-    talkThemes() {
+    async separateTalkThemesByCategory() {
+      this.fetchCategories();
+
       const selected_category = this.categories.find(
         (category) => category.id == this.category_id
       );
+
+      // 選択したカテゴリーのカテゴリー名とトークテーマをdataプロパティに代入する。
       this.category_name = selected_category.name;
       this.talk_themes = selected_category.talk_themes;
+
+      // 選択したカテゴリーのトークテーマを別のdataプロパティに再代入する。
+      this.filtered_talk_themes = this.talk_themes;
+    },
+
+    // 検索したトークテーマのデータをdataプロパティに代入する。
+    getFilteredTalkThemes(value) {
+      this.filtered_talk_themes = value;
     },
   },
 };
