195f53579fa0a706bb55496ef23a72a396eefb5f
icchan-webapp <ikkun.official@gmail.com>
2023-03-31 13:17:12 +0900

    refs #70 refactor: 不要になった関数、コンポーネント、CSSの記述を削除。

--- app/javascript/end_user/RoulettePage.vue
+++ app/javascript/end_user/RoulettePage.vue
@@ -1,11 +1,6 @@
 <template>
   <Header>B I L B I L</Header>
   <main class="container my-5">
-    <modal-window
-      :show_content="show_content"
-      :roulette_type="roulette_type"
-      @closeModal="closeModal"
-    ></modal-window>
     <div class="row mx-2">
       <div class="col-md-7 mx-auto">
         <!-- ページ遷移リンク -->
@@ -28,7 +23,7 @@
           <talk-theme-roulette></talk-theme-roulette>
         </div>
         <!-- /トークテーマルーレット -->
-        <!-- トークサポートルーレット -->
+        <!-- トークサポート -->
         <div class="mb-5">
           <head-line>TALK SUPPORT</head-line>
           <form @submit.prevent="addName">
@@ -44,54 +39,8 @@
               <button @click="deleteName(index)">Delete</button>
             </li>
           </ul>
-          <div class="mb-1">トーク人数を選んでください。</div>
-          <div class="d-flex flex-wrap mb-4">
-            <div class="number_btn-wrapper" v-for="n in 9" :key="n">
-              <input
-                type="radio"
-                class="btn-check"
-                :id="n + 1"
-                :value="n + 1"
-                v-model="number_of_people"
-              />
-              <label
-                class="btn btn-outline-secondary number_btn"
-                :for="n + 1"
-                >{{ n + 1 }}</label
-              >
-            </div>
-          </div>
-          <!-- /トーク人数選択ボタン -->
-          <!-- はてなボタン説明 -->
-          <div class="mb-3">
-            はてなボタンを押すと、各ルーレットの使い方を見ることができます。
-          </div>
-          <!-- /はてなボタン説明 -->
-          <!-- 番号指定ルーレット -->
-          <number-assignment-roulette
-            @openModal="openModal"
-          ></number-assignment-roulette>
-          <!-- /番号指定ルーレット -->
-          <div class="row">
-            <!-- トーク順番ルーレット -->
-            <div class="col-12 col-lg-5 mb-3 me-lg-auto mb-lg-0">
-              <talk-order-roulette
-                :number_of_people="number_of_people"
-                @openModal="openModal"
-              ></talk-order-roulette>
-            </div>
-            <!-- /トーク順番ルーレット -->
-            <!-- 司会者・話し手指定ルーレット -->
-            <div class="col-12 col-lg-6">
-              <host-roulette
-                :number_of_people="number_of_people"
-                @openModal="openModal"
-              ></host-roulette>
-            </div>
-            <!-- /司会者・話し手指定ルーレット -->
-          </div>
         </div>
-        <!-- /トークサポートルーレット -->
+        <!-- /トークサポート -->
       </div>
     </div>
   </main>
@@ -102,47 +51,25 @@
 </template>
 
 <script>
-import ModalWindow from "../components/ModalWindow.vue";
 import Header from "../components/Header.vue";
 import HeadLine from "../components/Headline.vue";
 import TalkThemeRoulette from "../components/TalkThemeRoulette.vue";
-import NumberAssignmentRoulette from "../components/NumberAssignmentRoulette.vue";
-import TalkOrderRoulette from "../components/TalkOrderRoulette.vue";
-import HostRoulette from "../components/HostRoulette.vue";
 import EndUserFooter from "../components/EndUserFooter.vue";
 
 export default {
   components: {
-    ModalWindow,
     Header,
     HeadLine,
     TalkThemeRoulette,
-    NumberAssignmentRoulette,
-    TalkOrderRoulette,
-    HostRoulette,
     EndUserFooter,
   },
   data() {
     return {
-      number_of_people: 2, // 選択されているトーク人数（デフォルトは2人）。
-      show_content: false, // モーダルの表示の有無。
-      roulette_type: "", // ルーレットの種類。
       new_name: "", // フォームに入力された名前。
       participants: JSON.parse(localStorage.getItem("participants")) || [], // トーク参加者の名前の配列。
     };
   },
   methods: {
-    // どのルーレットかを判断し、そのルーレットの説明が書かれたモーダルウィンドウを表示する。
-    openModal(roulette_type) {
-      this.show_content = true;
-      this.roulette_type = roulette_type;
-    },
-
-    // モーダルウィンドウを閉じる。
-    closeModal() {
-      this.show_content = false;
-    },
-
     addName() {
       // 新しい名前を追加する。
       this.participants.push({ name: this.new_name });
@@ -165,12 +92,6 @@ export default {
 </script>
 
 <style>
-.modal_btn {
-  padding: 5px 10px;
-  border: solid 1px #6c757d;
-  cursor: pointer;
-}
-
 .roulette {
   border: 1px solid #000;
   padding: 0.5rem;
